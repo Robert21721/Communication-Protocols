8a136e958eb2d2d374072e99f1b48295964a7759
Robert21721 <robert22stoica21@gmail.com>
2023-03-28 01:05:59 +0300

    draci

--- Tema1/router.c
+++ Tema1/router.c
@@ -37,71 +37,64 @@ struct arp_entry *get_mac_entry(uint32_t ip_dest) {
 }
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char buf[MAX_PACKET_LEN];
 
 	// Do not modify this line
 	init(argc - 2, argv + 2);
 
 	// de aici incepe chinu
-	int interface;
-    int len;
-
-	 rtable = malloc(sizeof(struct route_table_entry) * 100000);
-    /* DIE is a macro for sanity checks */
+	rtable = malloc(sizeof(struct route_table_entry) * 100000);
     DIE(rtable == NULL, "memory");
 
     mac_table = malloc(sizeof(struct arp_entry) * 100000);
     DIE(mac_table == NULL, "memory");
 
-    /* Read the static routing table and the MAC table */
     rtable_len = read_rtable(argv[1], rtable);
     mac_table_len = parse_arp_table("arp_table.txt", mac_table);
 
-
 	while (1) {
-
 		int interface;
 		size_t len;
 
 		interface = recv_from_any_link(buf, &len);
 		DIE(interface < 0, "recv_from_any_links");
 
-		struct ether_header *eth_hdr = (struct ether_header *) buf;
-		struct iphdr *ip_hdr = (struct iphdr *)(buf + sizeof(struct ether_header));
-		/* Note that packets received are in network order,
-		any header field which has more than 1 byte will need to be conerted to
-		host order. For example, ntohs(eth_hdr->ether_type). The oposite is needed when
-		sending a packet on the link, */
-
-		if (ip_hdr->check != ip_hdr->check + checksum((void*)ip_hdr, sizeof(struct iphdr)))
-            continue;
-            
-        printf("Check is good\n");
+		struct ether_header *eth_hdr = (struct ether_header*) buf;
+		struct iphdr *ip_hdr = (struct iphdr*)(buf + sizeof(struct ether_header));
+        struct icmphdr *icmp_hdr = (struct icmphdr*)(buf + sizeof(struct ether_header) + sizeof(struct iphdr));
+		
         
-        /* TODO 2.2: Call get_best_route to find the most specific route, continue; (drop) if null */
+		if (checksum((void*)ip_hdr, sizeof(struct iphdr)))
+            continue;
+        
         struct route_table_entry* best_route = get_best_route(ip_hdr->daddr);
-        if (best_route == NULL)
-            continue;
 
-        /* TODO 2.3: Check TTL >= 1. Update TLL. Update checksum using the incremental forumla  */
+        if (ip_hdr->ttl <= 1) {
+            icmp_hdr->type = 11;
+            icmp_hdr->code = 0;
+            ip_hdr->daddr = ip_hdr->saddr;
+            best_route = get_best_route(ip_hdr->daddr);
+            // ip_hdr->protocol = 1;
+            continue;
+        }
+
+        if (best_route == NULL) {
+            icmp_hdr->type = 3;
+            icmp_hdr->code = 0;
+            ip_hdr->daddr = ip_hdr->saddr;
+            best_route = get_best_route(ip_hdr->daddr);
+            // ip_hdr->protocol = 1;
+            continue;
+        }
+
         ip_hdr->ttl--;
-        if (ip_hdr->ttl <= 0) 
-            continue;
+        ip_hdr->check = ~(~ip_hdr->check - 1);
 
-        ip_hdr->check = ~(~ip_hdr->check + ~(ip_hdr->ttl + 1) + ip_hdr->ttl) - 1;
-
-        /* TODO 2.4: Update the ethernet addresses. Use get_mac_entry to find the destination MAC
-                 * address. Use get_interface_mac(m.interface, uint8_t *mac) to
-                 * find the mac address of our interface. */
         memcpy(eth_hdr->ether_dhost, get_mac_entry(best_route->next_hop)->mac, sizeof(uint8_t) * 6);
         get_interface_mac(best_route->interface, eth_hdr->ether_shost);
-        
 
-        // Call send_to_link(best_router->interface, packet, len);
         send_to_link(best_route->interface, buf, len);
-
 	}
 
 	return 0;
